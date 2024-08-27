e38ad6ec65a7a23bad034e0830d339978d036a64
LyraMaker <gabriel.lyrat@gmail.com>
2024-08-27 16:37:30 +0000

    27-08 | Correção do desafio

---
+++ Programação Web/desafio/Produto.php
@@ -0,0 +1,52 @@
+<?php
+
+class Produto
+{
+    private string $codigo;
+    private string $nome;
+    private float $preco;
+    private int $quantidade = 0; //Valor inicial definido como 0, pois não há como realizar soma em valores não definidos! 😁
+
+    public function __construct(string $codigo, string $nome, float $preco, int $quantidade){
+        $this->setCodigo($codigo);
+        $this->setNome($nome);
+        $this->setPreco($preco);
+        $this->adicionarEstoque($quantidade);
+    }
+    public function getCodigo(): string
+    {
+        return $this->codigo;
+    }
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+    public function getPreco(): string
+    {
+        return $this->preco;
+    }
+    public function getQuantidade(): string
+    {
+        return $this->quantidade;
+    }
+    public function setPreco(float $novoPreco)
+    {
+        $this->preco = $novoPreco;
+    }
+    public function setNome(string $nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function setCodigo(string $codigo){
+        $this->codigo = $codigo;
+    }
+    public function adicionarEstoque(int $quantidade)
+    {
+        $this->quantidade += $quantidade;
+    }
+    public function removerEstoque(int $quantidade)
+    {
+        $this->quantidade -= $quantidade;
+    }
+}
