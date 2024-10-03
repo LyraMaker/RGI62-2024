af0fd5c2eb68fb689f6f52ea4ec3dedec4ccaa4f
LyraMaker <gabriel.lyrat@gmail.com>
2024-10-03 13:54:29 -0300

    03/10 - Cadastrando no banco

--- Programação Web/reforço/app/Model/AlunoBanco.php
+++ Programação Web/reforço/app/Model/AlunoBanco.php
@@ -12,7 +12,21 @@ class AlunoBanco{
         $sql = "SELECT * FROM alunos";
         $resultado = $this->pdo->query($sql);
         $listaDoBanco = $resultado->fetchAll(PDO::FETCH_ASSOC);
-        return $this->hidratar( $listaDoBanco);
+        return $this->hidratar($listaDoBanco);
+    }
+
+    public function cadastrarAluno($matricula, $nome, $nota1, $nota2){
+        $sql = "INSERT INTO alunos(matricula, nome, media1, media2) VALUES (:m,:n,:m1,:m2)";
+        $pdo = $this->pdo;
+
+        $comando = $pdo->prepare($sql);
+        $comando->bindValue("m",$matricula,PDO::PARAM_STR);
+        $comando->bindValue("n",$nome,PDO::PARAM_STR);
+        $comando->bindValue("m1",$nota1,PDO::PARAM_STR);
+        $comando->bindValue("m2",$nota2,PDO::PARAM_STR);
+
+        $comando->execute();
+        
     }
 
     public function hidratar(array $array){
